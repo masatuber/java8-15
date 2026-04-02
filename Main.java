6db989cd038eccef0709d3d3d533f0fbe0bc2b3b
masatuber <uvervivid1231@icloud.com>
2026-04-02 22:36:31 +0900

    インクリメント練習

--- Main.java
+++ Main.java
@@ -4,5 +4,15 @@ int x = 6;
 int y = x++;
 int z = ++y;
 System.out.println(x + "" + y + "" + z);
+
+
+
+
+// 問題以外
+int A = 1;
+int A2 = A++; //先に変数に格納されて加算しない
+System.out.println(A2 + "：A2です");
+int B = 3; B++; // セミコロンで文の命令を終了でインクリメントされる System.out.println(B++);は加算されない
+System.out.println(B);
 }
 } 
